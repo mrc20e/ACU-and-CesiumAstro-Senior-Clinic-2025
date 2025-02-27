dcd3e269a9df10ac0be5b3495556c35cc1330ce9
Matson <matsoncamp@gmail.com>
2025-02-27 09:58:53 -0600

    Format JSON output with indentation and add stop signal to socket communication

--- formattingtest.py
+++ formattingtest.py
@@ -19,16 +19,17 @@ totaljson = json.dumps(total.tolist())
 dict = {"Time":time.tolist()[0], "Keys":["Data_Key1", "Data_Key2", "Data_Key3", "Data_Key4"], "Values":total.tolist()}
 #print(dict)
 
-jsondata = json.dumps(dict)
-print(jsondata)
+jsondata = json.dumps(dict, indent=4)
+#print(jsondata)
 
 dict2 = {"Time":time.tolist()[0], "Key1":total.tolist()[0], "Key2":total.tolist()[1], "Key3":total.tolist()[2], "Key4":total.tolist()[3]}
 #print(dict2)
 
 jsondata2 = json.dumps(dict2, indent=4)
-print(jsondata2)
+#print(jsondata2)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.sendto(jsondata.encode(), (socket.gethostname(), 8080))
 s.sendto(jsondata2.encode(), (socket.gethostname(), 8080))
+s.sendto("stop".encode(), (socket.gethostname(), 8080))
